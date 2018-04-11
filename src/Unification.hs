6165ca2803dbb1a9f80eab369ea61d19cd439773
Vadim Semenov <vadim.pillarbox@gmail.com>
2018-04-12 01:01:14 +0300

    Add unification algorithm and expression parser

---
+++ src/Unification.hs
@@ -0,0 +1,127 @@
+module Unification
+       ( Equation (..)
+       , Term (..)
+       , Literal
+       , FunctionLiteral
+       , VariableLiteral
+       , unify
+       ) where
+
+
+import qualified Data.Text as T
+import Control.Monad (join)
+import Debug.Trace (traceShowId)
+
+
+data Equation = Term :=: Term
+    deriving (Show, Eq)
+infixl 8 :=:
+
+data Term = Function FunctionLiteral [Term] | Variable VariableLiteral
+    deriving (Show, Eq)
+
+type Literal = T.Text
+type FunctionLiteral = Literal
+type VariableLiteral = Literal
+
+type EquationSystem = [Equation]
+
+{-
+Curry-Howard Isomorphism by Morten Heine B. Sorensen and Pawel Urzyczyn (page 94)
+
+a) Replace "x = t" and "x = s" (where t is not a variable) by "x = t" and "t = s";
+b) Replace "t = x" by "x = t";
+c) Replace "f(t1, ..., tn) = f(u1, ..., un)" by "t1 = u1", ..., "tn = un";
+d) Replace "x = t" and "r = s" by "x = t" and "r[x := t] = s[x := t]";
+e) Remove an equation of the form "t = t".
+-- -}
+
+unify :: EquationSystem -> Maybe EquationSystem
+unify system = unificationStep system >>= \system' ->
+    if system == system' then Just system else unify system'
+
+unificationStep :: EquationSystem -> Maybe EquationSystem
+unificationStep system = checkValid system >>=
+                         swapAll . traceShowId >>=
+                         filterLeftAll . traceShowId >>=
+                         unrollAll . traceShowId >>=
+                         substituteAll . traceShowId >>=
+                         filterEqualsAll . traceShowId
+
+-- Check absence of "x = t" where t contains x
+checkValid :: EquationSystem -> Maybe EquationSystem
+checkValid system = traverse checkValid' system
+  where
+    checkValid' eq@((Variable var) :=: f@(Function _ _)) = if member var f then Nothing else Just eq
+    checkValid' eq@(f@(Function _ _) :=: (Variable var)) = if member var f then Nothing else Just eq
+    checkValid' eq                                       = Just eq
+
+    member var (Variable var')   = var == var'
+    member var (Function _ args) = any (member var) args
+
+-- a)
+filterLeftAll :: EquationSystem -> Maybe EquationSystem
+filterLeftAll system = Just $ go system
+  where
+    go :: EquationSystem -> EquationSystem
+    go [] = []
+    go (h : t) = h : (go (filterLeft h t))
+
+filterLeft :: Equation -> EquationSystem -> EquationSystem
+filterLeft ((Variable var) :=: t) system = map replaceLeft system
+  where
+    replaceLeft :: Equation -> Equation
+    replaceLeft (v@(Variable var') :=: s) = (if var' == var then t else v) :=: s
+    replaceLeft eq                        = eq
+filterLeft _ system = system
+
+-- b)
+swapAll :: EquationSystem -> Maybe EquationSystem
+swapAll system = Just $ map swapIfNeeded system
+
+swapIfNeeded :: Equation -> Equation
+swapIfNeeded (f@(Function _ _) :=: v@(Variable _)) = v :=: f
+swapIfNeeded eq                                    = eq
+
+-- c)
+unrollAll :: EquationSystem -> Maybe EquationSystem
+unrollAll system = join <$> traverse unrollFunction system
+
+unrollFunction :: Equation -> Maybe EquationSystem
+unrollFunction ((Function f fArgs) :=: (Function g gArgs)) =
+    if f == g && length fArgs == length gArgs
+    then Just $ map (uncurry (:=:)) $ zip fArgs gArgs
+    else Nothing
+unrollFunction eq = Just [eq]
+
+-- d)
+substituteAll :: EquationSystem -> Maybe EquationSystem
+substituteAll system = substitute' [] system
+    where
+      substitute' pred [] = Just $ reverse pred
+      substitute' pred (eq@((Variable var) :=: t) : rest) = if lhs == pred && rhs == rest
+                                                            then substitute' (eq : pred) rest
+                                                            else Just $ (reverse lhs) ++ (eq : rhs)
+        where
+          substitution = (var, t)
+          (lhs, rhs) = (substituteEverywhere substitution pred, substituteEverywhere substitution rest)
+
+substituteEverywhere :: (VariableLiteral, Term) -> EquationSystem -> EquationSystem
+substituteEverywhere substitution = map (substituteEquation substitution)
+
+substituteEquation :: (VariableLiteral, Term) -> Equation -> Equation
+substituteEquation substitution (lhs :=: rhs) = (substitute substitution lhs :=: substitute substitution rhs)
+
+substitute :: (VariableLiteral, Term) -> Term -> Term
+substitute (instead, what) = substitute'
+  where
+    substitute' var@(Variable name)      = if name == instead then what else var
+    substitute' fun@(Function name args) = Function name $ map substitute' args
+
+-- e)
+filterEqualsAll :: EquationSystem -> Maybe EquationSystem
+filterEqualsAll system = Just $ filter equalityFilter system
+
+equalityFilter :: Equation -> Bool
+equalityFilter (lhs :=: rhs) = lhs /= rhs
+
---
+++ src/UnificationParser.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module UnificationParser
+       ( Parser
+       , parseEquation
+       ) where
+
+import Unification
+
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+import           Control.Monad              (void)
+import qualified Data.Text                  as T
+import           Data.Void
+
+
+type Parser = Parsec Void T.Text
+
+
+parseEquation :: T.Text -> Either (ParseError Char Void) Equation
+parseEquation = parse (equation <* eof) ""
+
+equation :: Parser Equation
+equation = do
+  lhs <- term
+  _   <- eq
+  rhs <- term
+  return $ lhs :=: rhs
+
+term :: Parser Term
+term = function <|> variable
+
+function :: Parser Term
+function = lexeme $ do
+    head <- satisfy (\c -> 'a' <= c && c <= 'h')
+    tail <- literalTail
+    let functionName = T.cons head tail
+    args <- parens $ sepBy1 term comma
+    return $ Function functionName args
+
+variable :: Parser Term
+variable = lexeme $ do
+    head <- satisfy (\c -> 'i' <= c && c <= 'z')
+    tail <- literalTail
+    return $ Variable $ T.cons head tail
+
+literalTail :: Parser T.Text
+literalTail = T.pack <$> many (try lowerChar <|> try digitChar <|> try (char '\''))
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme space
+
+symbol :: T.Text -> Parser T.Text
+symbol = L.symbol space
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (char ')')
+
+comma :: Parser ()
+comma = void $ symbol ","
+
+eq :: Parser ()
+eq = void $ symbol "="
